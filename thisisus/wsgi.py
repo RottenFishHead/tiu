3e69d397ddc32889de6aed0ee87fcc3e278cae13
Karen Sonnenberg <kjsonnenberg@me.com>
2025-12-20 13:50:42 -0800

    wsgi files changed

--- thisisus/wsgi.py
+++ thisisus/wsgi.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for thisisus project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+project_home = 'home/RottenFishHead/tiu'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
 
 from django.core.wsgi import get_wsgi_application
 
