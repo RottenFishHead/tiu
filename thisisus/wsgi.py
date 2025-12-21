bc023908484710c21a422670aa0266b8a46cf4c1
Karen Sonnenberg <kjsonnenberg@me.com>
2025-12-21 08:50:34 -0800

    changes to wsgi

--- thisisus/wsgi.py
+++ thisisus/wsgi.py
@@ -1,10 +1,5 @@
 import os
-import sys
 
-# Add your project directory to the sys.path
-project_home = '/home/RottenFishHead/tiu'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
 
 from django.core.wsgi import get_wsgi_application
 
