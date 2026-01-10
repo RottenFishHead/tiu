d5e95603e6a423a07dbffa497c1b5484a3d3beee
Karen Sonnenberg <kjsonnenberg@me.com>
2026-01-10 03:24:15 -0800

    added migration

--- thisisus/settings.py
+++ thisisus/settings.py
@@ -6,16 +6,16 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SECRET_KEY="c56e9087ac95dd75"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
-if not SECRET_KEY:
-    raise ValueError("The SECRET_KEY environment variable is not set.")
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# if not SECRET_KEY:
+#     raise ValueError("The SECRET_KEY environment variable is not set.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
